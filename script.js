017e4353c867d9a21c9c93966db182f997ec8e28
JacksonJ97 <jacksonjude97@gmail.com>
2021-08-07 01:29:34 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,69 +1,58 @@
+const player = (marker) => {
+  return { marker };
+};
+
 const gameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
 
   const setSquare = (index, marker) => {
-    if (index > board.length) return;
     board[index] = marker;
-  };
-
-  const getSquare = (index) => {
-    if (index > board.length) return;
-    return board[index];
   };
 
   const reset = () => {
     for (let i = 0; i < board.length; i++) {
       board[i] = "";
     }
+
+    gameController.winnerDeclared = false;
   };
 
-  return { board, setSquare, getSquare, reset };
+  return { board, setSquare, reset };
 })();
 
 const displayController = (() => {
-  const gameBoardSquares = document.querySelectorAll(".gameboard-square");
-  const gameBoardSquaresArr = Array.from(gameBoardSquares);
+  const gameBoardSquareElements = document.querySelectorAll(".gameboard-square");
+  const gameBoardSquareElementsArr = Array.from(gameBoardSquareElements);
 
   const render = () => {
     for (let i = 0; i < gameBoard.board.length; i++) {
-      gameBoardSquaresArr[i].textContent = gameBoard.board[i];
+      gameBoardSquareElementsArr[i].textContent = gameBoard.board[i];
     }
   };
 
-  gameBoardSquares.forEach((square) => {
+  gameBoardSquareElements.forEach((square) => {
     square.addEventListener("click", (e) => {
       let index = e.target.getAttribute("data-index");
 
-      if (gameController.activePlayer === "playerX" && gameBoard.board[index] === "") {
-        gameBoard.setSquare(index, gameController.playerX.getMarker());
-        gameController.activePlayer = "playerO";
-      } else if (gameController.activePlayer === "playerO" && gameBoard.board[index] === "") {
-        gameBoard.setSquare(index, gameController.playerO.getMarker());
-        gameController.activePlayer = "playerX";
-      }
+      gameController.playRound(index);
+      render();
 
       if (gameController.checkXWinner()) {
+        gameController.winnerDeclared = true;
         console.log("playerX wins");
-        gameBoard.reset();
       } else if (gameController.checkOWinner()) {
+        gameController.winnerDeclared = true;
         console.log("playerO wins");
-        gameBoard.reset();
       }
 
-      render();
+      if (gameController.winnerDeclared) {
+        gameBoard.reset();
+      }
     });
   });
 
-  return { render, gameBoardSquaresArr };
+  return { render };
 })();
-
-const player = (marker) => {
-  this.marker = marker;
-
-  const getMarker = () => marker;
-
-  return { getMarker };
-};
 
 const gameController = (() => {
   const playerX = player("X");
@@ -82,22 +71,35 @@ const gameController = (() => {
     [2, 4, 6],
   ];
 
+  const playRound = (index) => {
+    if (activePlayer === "playerX" && gameBoard.board[index] === "") {
+      gameBoard.setSquare(index, playerX.marker);
+      activePlayer = "playerO";
+    } else if (activePlayer === "playerO" && gameBoard.board[index] === "") {
+      gameBoard.setSquare(index, playerO.marker);
+      activePlayer = "playerX";
+    }
+  };
+
   const checkXWinner = () => {
+    // Will return true if one combination is satisfied
     return winConditions.some((condition) => {
-      // Will return true if one combination is satisfied
+      // Will return true if all indices in the combination array is equal to X
       return condition.every((index) => {
-        return displayController.gameBoardSquaresArr[index].textContent === "X"; // Will return true if all indices in the combination array is equal to X
+        return gameBoard.board[index] === "X";
       });
     });
   };
 
   const checkOWinner = () => {
+    // Will return true if one combination is satisfied
     return winConditions.some((condition) => {
+      // Will return true if all indices in the combination array is equal to X
       return condition.every((index) => {
-        return displayController.gameBoardSquaresArr[index].textContent === "O";
+        return gameBoard.board[index] === "O";
       });
     });
   };
 
-  return { checkXWinner, checkOWinner, activePlayer, playerX, playerO };
+  return { playRound, checkXWinner, checkOWinner, winnerDeclared };
 })();
