1b20e872c1c35fd70b00803254e68256d398bc72
JacksonJ97 <jacksonjude97@gmail.com>
2021-08-06 23:48:21 -0400

    players can place markers on the tic tac toe grid

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
 const gameBoard = (() => {
-  const gameBoardArr = ["X", "X", "O", "O", "O", "X", "X", "X", "X"];
+  const board = ["", "", "", "", "", "", "", "", ""];
 
-  const render = () => {
-    const gameBoardSquares = document.querySelectorAll(".gameboard-square");
-    const gameBoardSquaresArr = Array.from(gameBoardSquares);
+  const setSquare = (index, symbol) => {
+    if (index > board.length) return;
+    board[index] = symbol;
+  };
 
-    for (let i = 0; i < gameBoardArr.length; i++) {
-      gameBoardSquaresArr[i].textContent = gameBoardArr[i];
+  const getSquare = (index) => {
+    if (index > board.length) return;
+    return board[index];
+  };
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
     }
   };
+
+  return { board, setSquare, getSquare, reset };
+})();
+
+const displayController = (() => {
+  const gameBoardSquares = document.querySelectorAll(".gameboard-square");
+  const gameBoardSquaresArr = Array.from(gameBoardSquares);
+
+  const render = () => {
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      gameBoardSquaresArr[i].textContent = gameBoard.board[i];
+    }
+  };
+
+  gameBoardSquares.forEach((square) => {
+    square.addEventListener("click", (e) => {
+      let index = e.target.getAttribute("data-index");
+      gameBoard.setSquare(index, playerX.getSymbol());
+      console.log(gameBoard.board);
+      render();
+    });
+  });
 
   return { render };
 })();
 
-gameBoard.render();
+const player = (symbol) => {
+  this.symbol = symbol;
+
+  const getSymbol = () => symbol;
+
+  return { getSymbol };
+};
+
+// const gameController = (() => {
+//   let playerX = player("X");
+//   let playerO = player("O");
+// })();
+
+let playerX = player("X");
+// displayController.render();
